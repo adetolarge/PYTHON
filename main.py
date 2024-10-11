483c9725c58d84a3ac10bc85adfccb1244dd4999
adetolarge <adetolarge@gmail.com>
2024-10-11 12:59:44 +0100

    My 1st PHYTHON class

---
+++ main.py
@@ -0,0 +1,17 @@
+name = "Emmanuel"
+age = 23
+accountBalance = 234.5
+isAdmin = False
+
+userName = input('Please What is your name: ')
+#print(f'Welcome Mr. {userName}')
+#print(name)
+#print(age)
+#print(accountBalance)
+#print(isAdmin)
+ 
+print(2+3)
+print(4-1)
+print(3*4)
+print(4**5)
+print(16/4)  
