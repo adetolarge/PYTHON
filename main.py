5db47101fb177977b47150aad94ff0c6a7391f7d
adetolarge <adetolarge@gmail.com>
2024-10-15 10:49:34 +0100

    update

--- main.py
+++ main.py
@@ -3,16 +3,29 @@ age = 23
 accountBalance = 234.5
 isAdmin = False
 
-userName = input('Please What is your name: ')
+#userName = input('Please What is your name: ')
 #print(f'Welcome Mr. {userName}')
 #print(name)
 #print(age)
 #print(accountBalance)
 #print(isAdmin)
  
-print(2+3)
-print(4-1)
-print(3*4)
-print(4**5)
-print(16/4)  
+#print(2+3)
+#print(4-1)
+#print(3*4)
+#print(4**5)
+#print(16/4)  
 
+# name = input('Please What is your name: ')
+# age = (input("Enter your age: "))
+# intAge = int(age)
+# if intAge >= 18:
+#     print(f"{name} You are an Adult")
+# elif intAge >= 13:
+#     print(f"{name} You are a teenager")
+# else:
+#     print(f"{name} You are still a Child😁😁")
+
+# print(range(5))
+for number in range(1, 5):
+    print(number)
