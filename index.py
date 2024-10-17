27a2f75084d595e1416aec2c6e523015b787e868
adetolarge <adetolarge@gmail.com>
2024-10-17 10:52:59 +0100

    update

--- index.py
+++ index.py
@@ -7,16 +7,28 @@ favoriteColors.append("Brown")
 favoriteColors.pop(1)
 # print(favoriteColors)
 
-numbers = [3,2,4,4,5,7,3,5,66,77,43,20,15]
-numbers.sort(reverse = True)
-total_sum = sum(numbers)
-# print(sum(numbers))
+# numbers = [3,2,4,4,5,7,3,5,66,77,43,20,15]
+# numbers.sort(reverse = True)
+# total_sum = sum(numbers)
+# # print(sum(numbers))
 
-item = input("Enter five numbers separated by commas: ").split(",")
-print(f"Original List {item}")
-number_to_remove = input("Enter a number to remove: ")
-if number_to_remove in item:
-    item.remove(number_to_remove)
-    print(f"Updated list: {item}")
-else:
-    print(f"{number_to_remove} not found in the list.")
+# item = input("Enter five numbers separated by commas: ").split(",")
+# print(f"Original List {item}")
+# number_to_remove = input("Enter a number to remove: ")
+# if number_to_remove in item:
+#     item.remove(number_to_remove)
+    # print(f"Updated list: {item}")
+# else:
+    # print(f"{number_to_remove} not found in the list.")
+
+#Tuple
+# myTuple = (12.004, 34.4)
+# print(myTuple)
+# print(myTuple.index(34.4))
+
+# a, b = 5, 10
+# a, b = b, a
+# print(b,a)
+
+mySets = {2,23,4,4,5,32,4,5,6,4,6}
+print(mySets)
