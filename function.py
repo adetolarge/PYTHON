14c88c0365481cf640c675415293be62c69dcbf8
adetolarge <adetolarge@gmail.com>
2024-10-29 11:03:27 +0100

    functions in python

---
+++ function.py
@@ -0,0 +1,13 @@
+# def hello():
+#     print('Hello World')
+# def greetings():
+#     age = 50
+#     name = "Adetola"
+#     print(f'Hello {name} How are you? In 5 years time, you will be {age+5} years old. ')
+
+    #create a funtion that calculates the square root of a number
+def cube(number):
+    return number ** 3
+num = int(input("Please enter a number: "))
+result = cube(num)
+print(f"The cube of {num} is {result}")
