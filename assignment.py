7f319bb24455a04d635e527ec3a7287065f6d0d9
adetolarge <adetolarge@gmail.com>
2025-09-02 17:43:06 +0100

    new classes

---
+++ assignment.py
@@ -0,0 +1,25 @@
+# 1. Hospital Management System
+
+# Problem Statement: Design a program that allows hospital staff to manage patients and doctors.
+
+# Requirements:
+
+# Patient Class (encapsulated): name, age, ailment.
+
+# Doctor Class: name, specialty.
+
+# Hospital Class:
+
+# Private lists: __patients, __doctors, __appointments.
+
+# Methods: add_patient(), add_doctor(), schedule_appointment(), display_patients(), display_doctors(), display_appointments().
+# User Actions:
+
+# Add a patient/doctor.
+
+# Schedule an appointment.
+
+# View records.
+
+# Exit system.
+# .
--- function.py
+++ function.py
@@ -1,20 +1,16 @@
-# def hello():
-#     print('Hello World')
-# def greetings():
-#     age = 50
-#     name = "Adetola"
-#     print(f'Hello {name} How are you? In 5 years time, you will be {age+5} years old. ')
+def hello():
+    print('Hello World')
+def greetings():
+    age = 50
+    name = "Adetola"
+    print(f'Hello {name} How are you? In 5 years time, you will be {age+5} years old. ')
 
-    #create a funtion that calculates the square root of a number
+#create a funtion that calculates the square root of a number
 def cube(number):
     return number ** 3
 num = int(input("Please enter a number: "))
 result = cube(num)
-# print(f"The cube of {num} is {result}")
-
-
-
-
+print(f"The cube of {num} is {result}")
 #Assignment
 #Write a Python function that prompts the user to enter a number. Then, given that number, it prints the sentence "Hello, Python!" that many times.
 def random_number():
